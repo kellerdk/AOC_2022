a840201ae5b160643fc9c964ccaf01364f3bef49
David Keller <kellerdk@gmail.com>
2022-12-02 23:11:56 -0800

    cleanup

--- day3.py
+++ day3.py
@@ -8,21 +8,18 @@ def get_data(fname):
     with open(fname) as fptr:
         return [i.rstrip("\n") for i in fptr.readlines()]
 
-def common_line(s):
+def single(s):
     l = len(s)//2
     return priority[list(set(s[:l]) & set(s[l:]))[0]]
 
-def sumpriority(xs):
-    return sum(common_line(i) for i in xs)
-
-def common_triple(x, y, z):
+def triple(x, y, z):
     return priority[list(set(x) & set(y) & set(z))[0]]
 
-def sumtriple(xs):
-    ys = list(zip(*[xs[i::3] for i in range(3)]))
-    return sum(common_triple(*i) for i in ys)
+def sumlines(xs, n, f):
+    return sum(f(*i) for i in zip(*[xs[i::n] for i in range(n)]))
 
 
-xs = get_data("input/day3_input.txt")
-print("by line", sumpriority(xs))
-print("by triple", sumtriple(xs))
+if __name__ == "__main__":
+    xs = get_data("input/day3_input.txt")
+    print("by line", sumlines(xs, 1, single))
+    print("by triple", sumlines(xs, 3, triple))
