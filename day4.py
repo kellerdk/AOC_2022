40032b1c3ae3ea44ec4d4a5620c543eecafeda60
David Keller <kellerdk@gmail.com>
2022-12-03 21:52:18 -0800

    cleanup

--- day4.py
+++ day4.py
@@ -9,15 +9,19 @@ def to_range(xs):
     x0, x1 = xs.split("-")
     return set(range(int(x0), int(x1)+1))
 
-def subset(x1, x2):
+def proper_subset(x1, x2):
     u = to_range(x1)
     v = to_range(x2)
     return u.issubset(v) | v.issubset(u)
 
-def intersect(x1, x2):
+def subset(x1, x2):
     return list(to_range(x1) & to_range(x2)) != []
 
+def overlap_with(xs, f):
+    return sum((f(*i.split(","))) for i in xs if True)
 
-xs = get_data("input/day4_input.txt")
-print(sum((subset(*i.split(","))) for i in xs if True))
-print(sum(intersect(*i.split(",")) for i in xs if True))
+
+if __name__ == "__main__":
+    xs = get_data("input/day4_input.txt")
+    print(overlap_with(xs, proper_subset))
+    print(overlap_with(xs, subset))
