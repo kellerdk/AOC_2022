af2ade4d8d2e681c7fc8b23d4dd2c7908d1d1f47
David Keller <kellerdk@gmail.com>
2022-12-05 07:33:36 -0800

    light refactor

--- day5.py
+++ day5.py
@@ -5,7 +5,8 @@
 
 import re
 
-repat = re.compile(r"\d+")
+number = re.compile(r"\d+")
+chars = re.compile(r"([A-Z]+)(\d+)")
 
 
 def get_data(fname):
@@ -14,47 +15,44 @@ def get_data(fname):
     with open(fname, encoding="utf-8") as fptr:
         return fptr.read().split("\n")[:-1]
 
+def parse(xss):
+    init, instructions = xss[:9], xss[9:]
+    return parse_state(init), parse_moves(instructions)
 
-def crates():
+
+def parse_moves(xss):
+    """Parse move list
     """
-            [J]         [B]     [T]
-            [M] [L]     [Q] [L] [R]
-            [G] [Q]     [W] [S] [B] [L]
-    [D]     [D] [T]     [M] [G] [V] [P]
-    [T]     [N] [N] [N] [D] [J] [G] [N]
-    [W] [H] [H] [S] [C] [N] [R] [W] [D]
-    [N] [P] [P] [W] [H] [H] [B] [N] [G]
-    [L] [C] [W] [C] [P] [T] [M] [Z] [W]
-     1   2   3   4   5   6   7   8   9
-     """
-    cdx = {}
-    cdx[1] = 'DTWNL'
-    cdx[2] = 'HPC'
-    cdx[3] = 'JMGDNHPW'
-    cdx[4] = 'LQTNSWC'
-    cdx[5] = 'NCHP'
-    cdx[6] = 'BQWMDNHT'
-    cdx[7] = 'LSGJRBM'
-    cdx[8] = 'TRBVGWNZ'
-    cdx[9] = 'LPNDGW'
-    return {k: list(v) for k, v in cdx.items()}
+    return [k for k in [[int(j) for j in number.findall(i)] for i in xss] if k]
 
 
-def move_crates(state, moves):
+def parse_state(xss):
+    """Parse initial state of crates from input
+    """
+    state = ["".join(i) for i in list(zip(*xss))]
+    xxs = [chars.search(i).groups() for i in state if chars.search(i)]
+    return {int(k): list(v) for v, k in xxs}
+
+
+def move_crates(instate):
     """Move crates one at a time. FIFO semantics
     """
+    state, moves = parse(instate)
     for qty, src, dst in moves:
         while qty > 0:
             state[dst] = list(state[src].pop(0)) + state[dst]
             qty -= 1
+    return state
 
 
-def move_stack(state, moves):
+def move_stack(instate):
     """Move crates by stack of crates in one move. FILO semantics
     """
+    state, moves = parse(instate)
     for qty, src, dst in moves:
         state[dst] = state[src][:qty] + state[dst]
         state[src] = state[src][qty:]
+    return state
 
 
 def report_state(state):
@@ -63,24 +61,7 @@ def report_state(state):
     return "".join(v[0] for v in state.values())
 
 
-def parse_move(xss):
-    """Move description in the form:  (quantity, source, destination)
-    """
-    return [int(i) for i in repat.findall(xss)]
-
-
-def parse_moves(xss):
-    """Parse move list
-    """
-    moves = [parse_move(i) for i in xss]
-    return [i for i in moves if i]
-
-
 if __name__ == "__main__":
-    directions = get_data("input/day5_input.txt")[9:]
-    m = parse_moves(directions)
-
+    directions = get_data("input/day5_input.txt")
     for apply in [move_crates, move_stack]:
-        s = crates()
-        apply(s, m)
-        print(f"{apply.__name__}:", report_state(s))
+        print(f"{apply.__name__}:", report_state(apply(directions)))
